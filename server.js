d0d2870ea57c411bf2a9e82da1a07f79034eb20b
Tyler Petri <tyler_1701@hotmail.com>
2021-07-31 17:51:56 -0400

    heroku

--- server.js
+++ server.js
@@ -2,21 +2,22 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const storiesRoutes = require('./server/routes/stories');
-const usersRoutes = require('./server/routes/users');
-const tagsRoutes = require('./server/routes/tags');
-const imageRoutes = require('./server/routes/image-upload');
+// const usersRoutes = require('./server/routes/users');
+// const tagsRoutes = require('./server/routes/tags');
+// const imageRoutes = require('./server/routes/image-upload');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
 app.use('/api/', storiesRoutes);
-app.use('/api/', usersRoutes);
-app.use('/api/', tagsRoutes);
-app.use('/api/', imageRoutes);
+// app.use('/api/', usersRoutes);
+// app.use('/api/', tagsRoutes);
+// app.use('/api/', imageRoutes);
 
 app.listen(PORT, () =>
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)

ea331be659c5cf11eb53a9191da6245c83f5a7eb
Tyler Petri <tyler_1701@hotmail.com>
2021-08-02 13:38:13 -0400

    gsap

--- client/src/Pages/Welcome/welcome.jsx
+++ client/src/Pages/Welcome/welcome.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import gsap from 'gsap';
 
 import write from '../../assets/animated-book.gif';
 import read from '../../assets/read-animated.gif';
@@ -13,9 +14,32 @@ export default function Welcome() {
 
   useEffect(() => dispatch({ type: 'CLEAR_SELECTED_FILES' }), []);
 
+  useEffect(() => {
+    gsap.to('.welcome-to-span', {
+      duration: 1.5,
+      ease: 'elastic.out(.4,.2)',
+      fontSize: '3em',
+    });
+    gsap.to('.library-of-stories-span', {
+      duration: 2.5,
+      ease: 'elastic.out(.4,.2)',
+      fontSize: '7em',
+    });
+    gsap.to('.welcome-img', { delay: 2, opacity: 1, ease: 'power1.out' });
+    gsap.to('.welcome-img-books', { delay: 2, opacity: 1, ease: 'power1.out' });
+  }, []);
+
+  function killGSAP() {
+    gsap.to('.welcome-img', { opacity: 1 });
+    gsap.to('.welcome-img-books', { opacity: 1 });
+  }
+
   return (
-    <div className='welcome-wrapper'>
-      <div className='welcome-title'>De Fabulis</div>
+    <div className='welcome-wrapper' onClick={killGSAP}>
+      <div className='welcome-title'>
+        <span className='welcome-to-span'>Welcome to</span>
+        <span className='library-of-stories-span'>Library of Stories</span>
+      </div>
       <div className='welcome-card'>
         <div className='wBooksCont'>
           {' '}
